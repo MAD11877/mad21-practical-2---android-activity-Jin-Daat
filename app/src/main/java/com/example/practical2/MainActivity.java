2e402210f8abbff3316ed9c5fd2894a795b51f07
Jin Daat <s10222867@connect.np.edu.sg>
2022-04-27 16:43:49 +0800

    Added User class and method for follow button

--- app/src/main/java/com/example/practical2/MainActivity.java
+++ app/src/main/java/com/example/practical2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.practical2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Method to change text when follow button is clicked
+    public void followPerson(View view){
+        Button button = findViewById(R.id.follow);
+        String btnText = button.getText().toString();
+        if (btnText.equals("Follow")){
+            button.setText("Unfollow");
+        }
+        else{
+            button.setText("Follow");
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/practical2/User.java
@@ -0,0 +1,8 @@
+package com.example.practical2;
+
+public class User {
+    String name;
+    String description;
+    int id;
+    boolean followed;
+}
