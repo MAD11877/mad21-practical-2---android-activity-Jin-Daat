3f5c0640f5b07757299b83257698e4dee2cf9b5f
Jin Daat <s10222867@connect.np.edu.sg>
2022-04-30 17:12:23 +0800

    Added scrollview

--- app/src/main/java/com/example/practical2/MainActivity.java
+++ app/src/main/java/com/example/practical2/MainActivity.java
@@ -5,16 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    User newUser = new User(
+            "JINESE",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+            1,
+            false
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    User newUser = new User("Jin Daat", "testing", 1, false);
+        //Set User name
+        TextView userName = (TextView)findViewById(R.id.userName);
+        userName.setText(newUser.name);
+
+        //Set User description
+        TextView userDesc = (TextView)findViewById(R.id.userDesc);
+        userDesc.setText(newUser.description);
+    }
 
     //Method to change text when follow button is clicked
     public void followPerson(View view){
--- app/src/main/java/com/example/practical2/User.java
+++ app/src/main/java/com/example/practical2/User.java
@@ -1,11 +1,13 @@
 package com.example.practical2;
 
 public class User {
-    String name;
-    String description;
-    int id;
-    boolean followed;
-//    Constructor
+    public String name;
+    public String description;
+    public int id;
+    public boolean followed;
+
+
+    //    Constructor
     public User (String userName, String userDesc, int userId, boolean userFollow){
         name = userName;
         description = userDesc;
