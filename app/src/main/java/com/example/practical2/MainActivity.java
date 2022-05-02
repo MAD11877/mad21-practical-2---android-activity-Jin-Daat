4c4d16e31554f3e1783c34f844704f9fa29e64b1
Jin Daat <s10222867@connect.np.edu.sg>
2022-05-02 10:17:36 +0800

    Placed method inside onCreate()

--- app/src/main/java/com/example/practical2/MainActivity.java
+++ app/src/main/java/com/example/practical2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.practical2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -28,20 +29,25 @@ public class MainActivity extends AppCompatActivity {
         //Set User description
         TextView userDesc = (TextView)findViewById(R.id.userDesc);
         userDesc.setText(newUser.description);
-    }
 
-    //Method to change text when follow button is clicked
-    public void followPerson(View view){
         Button followBtn = findViewById(R.id.follow);
 
-        if (!newUser.followed){
-            followBtn.setText("Unfollow");
-            newUser.followed = true;
-        }
-        else{
-            followBtn.setText("Follow");
-            newUser.followed = false;
-        }
+        followBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Button clickFollowBtn = findViewById(R.id.follow);
+
+                if (!newUser.followed){
+                    clickFollowBtn.setText("Unfollow");
+                    newUser.followed = true;
+                }
+                else{
+                    clickFollowBtn.setText("Follow");
+                    newUser.followed = false;
+                }
+            }
+
+        });
     }
 
 }
